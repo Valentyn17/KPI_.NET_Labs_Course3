212390b2b701b691d23133504b9ca34f3b7438ec
Valentyn17 <valentyn2612@gmail.com>
2021-12-03 23:14:23 +0200

    Create Web Api app
    Implement data layer and business layer

---
+++ Web_API_app/Web_API_app.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Web_API_app;
+using Web_API_app.Controllers;
+
+namespace Web_API_app.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ Web_API_app/Web_API_app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_API_app.WebApiApplication" Language="C#" %>
